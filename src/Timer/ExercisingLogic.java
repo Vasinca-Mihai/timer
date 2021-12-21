800fae8e3847a015868070edeaf7bd85d5df2cf6
Vasinca-Mihai <m_vasinca@yahoo.com>
2021-12-21 16:35:03 +0200

    the interface is somewhat flickering but it works

--- src/Timer/ExercisingLogic.java
+++ src/Timer/ExercisingLogic.java
@@ -3,6 +3,10 @@ package Timer;
 import ext_lib.tinysound.Sound;
 import ext_lib.tinysound.TinySound;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.util.List;
 import java.util.Scanner;
@@ -12,10 +16,12 @@ public class ExercisingLogic {
     private static Sound sound_stop;
     private static Sound sound_beep;
     private static double volume = 1;
-    //private static InterfaceManager interfaceManager;
+    private static boolean localCheck = true;
+    private static int exerciseNr =0;
+    private static List<Excercice> set;
 
     public static void startExercising(){
-        List<Excercice> set = Set.getSet();
+        set = Set.getSet();
         //interfaceManager = InterfaceManager.getInstance();
         TinySound.init();
         SoundLoader.setSet(set);
@@ -23,10 +29,9 @@ public class ExercisingLogic {
         sound_stop = TinySound.loadSound(new File("./src/Resources/Sounds/stop2.wav"));
         sound_beep = TinySound.loadSound(new File("./src/Resources/Sounds/beep.wav"));
 
-        goTroughExercises(set);
 
-        sleep(1000);
-        TinySound.shutdown();
+        goTroughExercises();
+
     }
     public static void interruptedSleepSeconds(int seconds){
         for(int i=seconds;i>0;i--){
@@ -44,31 +49,65 @@ public class ExercisingLogic {
             Thread.sleep(millis);
         }catch (InterruptedException e){e.printStackTrace();}
     }
-    public static void goTroughExercises(List<Excercice> ex){
-        Scanner s = new Scanner(System.in);
-        for(int i=0; i< 2 /*ex.size()*/;i++){
+    public static void goTroughExercises(){
+        //Scanner s = new Scanner(System.in);
+        //for(int i=0; i< 2 /*ex.size()*/;i++){
             sleep(2000);
-            ex.get(i).getSoundFile().play(volume);
+            set.get(exerciseNr).getSoundFile().play(volume);
             //System.out.println("\tNext exercise: "+ex.get(i).getExName());
             //System.out.println("\t"+ex.get(i).getExDesc());
-            InterfaceManager.updateTextFiled(TextFiledName.M2_ExTitle,ex.get(i).getExName());
-            InterfaceManager.updateTextFiled(TextFiledName.M2_ExDescription,ex.get(i).getExDesc());
+            InterfaceManager.updateTextFiled(TextFiledName.M2_ExTitle,set.get(exerciseNr).getExName());
+            InterfaceManager.updateTextFiled(TextFiledName.M2_ExDescription,set.get(exerciseNr).getExDesc());
 
-            interruptedSleepSeconds(ex.get(i).getRestTime() - 2);
+            interruptedSleepSeconds(set.get(exerciseNr).getRestTime() - 2);
             sound_start.play(volume);
-            if(ex.get(i).isInTime()) {
-                interruptedSleepSeconds(ex.get(i).getExeWork());
+            if(set.get(exerciseNr).isInTime()) {
+                interruptedSleepSeconds(set.get(exerciseNr).getExeWork());
+                sound_stop.play(volume);
+                exerciseNr++;
+                if(exerciseNr<set.size()){
+                    goTroughExercises();
+                }
+                if(exerciseNr>=set.size()){
+                    terminateProgram();
+                }
             }else {
-                InterfaceManager.updateTextFiled(TextFiledName.M2_Counter,"Reps: "+ex.get(i).getExeWork());
-                System.out.println("Please press enter once done");
+                InterfaceManager.EnableDisableDoneButton(true);
+                InterfaceManager.updateTextFiled(TextFiledName.M2_Counter,"Reps: "+set.get(exerciseNr).getExeWork());
+                //System.out.println("Please press enter once done");
+                //localCheck = true;
+                //while (localCheck){
+                    //try {
+                    //    Thread.sleep(1000);
+                    //}catch (InterruptedException e){e.printStackTrace();}
+                    //System.out.println("here");
+                //}
+                //InterfaceManager.EnableDisableDoneButton(false);
                 //s.nextLine();
             }
-            sound_stop.play(volume);
-        }
+
+        //}
+    }
+
+    public static void terminateProgram(){
+        sleep(1000);
+        TinySound.shutdown();
     }
     public static void setVolume(double v){
         volume = v;
     }
+
+    public static void continueExLogic(){
+        sound_stop.play(volume);
+        InterfaceManager.EnableDisableDoneButton(false);
+        exerciseNr++;
+        if(exerciseNr<set.size()){
+            goTroughExercises();
+        }
+        if(exerciseNr>=set.size()){
+            terminateProgram();
+        }
+
+    }
 }
-//in action listener add a nextEx method and if the ex is automated call it after wqait
-//next ex repalces go trough ex and is recurring (sefl call)
+//in action listener to change local check
--- src/Timer/InterfaceManager.java
+++ src/Timer/InterfaceManager.java
@@ -5,13 +5,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class InterfaceManager implements ActionListener{
-    static InterfaceManager instance;
+public class InterfaceManager implements ActionListener {
 
     private static JFrame window;
     private static JButton m1Start;
@@ -19,18 +20,21 @@ public class InterfaceManager implements ActionListener{
     private static JLabel m1FileInput;
     private static JTextField m1FileInputTF;
     private static JTextField m1NrOfSetsTF;
-    private static JTextField m1StartAtTF;
+    //private static JTextField m1StartAtTF;
     private static JLabel m1NrOfSets;
-    private static JLabel m1StartAt;
+    //private static JLabel m1StartAt;
+    //private static MyKeyListener KL;
 
     private static JLabel m2ExTitle;
     private static JLabel m2ExDescription;
     private static JLabel m2Counter;
+    private static JButton m2Done;
 
     private static List<Component> menu1;
     private static List<Component> menu2;
 
     public static void main(String[] args) {
+        //KL = new MyKeyListener();
         new InterfaceManager();
     }
 
@@ -42,6 +46,7 @@ public class InterfaceManager implements ActionListener{
 
     public InterfaceManager(){
         window = new JFrame();
+        //window.addKeyListener(KL);
         window.setSize(640,500);
         window.setLocationRelativeTo(null);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,7 +57,7 @@ public class InterfaceManager implements ActionListener{
         initiateM2();
         switchMenu(1);
 
-        window.setResizable(true);
+        window.setResizable(false);
         window.setVisible(true);
     }
 
@@ -66,12 +71,12 @@ public class InterfaceManager implements ActionListener{
             }else{
                 JOptionPane.showMessageDialog(new JFrame(),"No such file found.","404",JOptionPane.INFORMATION_MESSAGE);
             }
-            //System.out.println(m1FileInputTF.getText());
-            //System.out.println(m1NrOfSetsTF.getText());
-            //System.out.println(m1StartAtTF.getText());
         }else if(e.getSource() == m1Start) {
             switchMenu(2);
             ExercisingLogic.startExercising();
+        }else if(e.getSource() == m2Done){
+            ExercisingLogic.continueExLogic();
+            System.out.println("here");
         }
     }
 
@@ -98,34 +103,37 @@ public class InterfaceManager implements ActionListener{
         m1NrOfSetsTF.setLocation(410,10 + 60 + 10);
         m1NrOfSetsTF.setSize(200,60);
 
-        m1StartAt = new JLabel("Input the exercise to start at:");
-        m1StartAt.setToolTipText("This field is meant to be used in case something interrupted your workout (default: 0)");
-        m1StartAt.setFont(m1FileInput.getFont());
-        m1StartAt.setSize(m1FileInput.getSize());
-        m1StartAt.setHorizontalAlignment(JLabel.RIGHT);
-        m1StartAt.setLocation(10,10 + (60 + 10)*2);
-
-        m1StartAtTF = new JTextField();
-        m1StartAtTF.setFont(m1FileInput.getFont());
-        m1StartAtTF.setLocation(410,10 + (60 + 10)*2);
-        m1StartAtTF.setSize(200,60);
+        //m1StartAt = new JLabel("Input the exercise to start at:");
+        //m1StartAt.setToolTipText("This field is meant to be used in case something interrupted your workout (default: 0)");
+        //m1StartAt.setFont(m1FileInput.getFont());
+        //m1StartAt.setSize(m1FileInput.getSize());
+        //m1StartAt.setHorizontalAlignment(JLabel.RIGHT);
+        //m1StartAt.setLocation(10,10 + (60 + 10)*2);
+//
+        //m1StartAtTF = new JTextField();
+        //m1StartAtTF.setFont(m1FileInput.getFont());
+        //m1StartAtTF.setLocation(410,10 + (60 + 10)*2);
+        //m1StartAtTF.setSize(200,60);
 
         m1Validate = new JButton();
         m1Validate.setSize(600,60);
         m1Validate.setFont(m1FileInput.getFont());
         m1Validate.setText("Validate imputed data.");
-        m1Validate.setLocation(10,10+(60+10)*3);
+        m1Validate.setLocation(10,10+(60+10)*2);
         m1Validate.addActionListener(this);
 
         m1Start = new JButton();
         m1Start.setSize(600,60);
         m1Start.setFont(m1FileInput.getFont());
         m1Start.setText("Start exercising.");
-        m1Start.setLocation(10,10+(60+10)*4);
+        m1Start.setLocation(10,10+(60+10)*3);
         m1Start.setEnabled(false);
         m1Start.addActionListener(this);
 
-        menu1 = new ArrayList<>(Arrays.asList(m1FileInput,m1StartAt,m1NrOfSets,m1Start,m1FileInputTF,m1Validate,m1NrOfSetsTF,m1StartAtTF));
+        menu1 = new ArrayList<>(Arrays.asList(m1FileInput,m1NrOfSets,m1Start,m1FileInputTF,m1Validate,m1NrOfSetsTF));
+        //for(int i=0;i<menu1.size();i++){
+        //    menu1.get(i).addKeyListener(KL);
+        //}
     }
 
     private void initiateM2(){
@@ -139,19 +147,32 @@ public class InterfaceManager implements ActionListener{
         m2ExDescription = new JLabel();
         m2ExDescription.setLocation(0,120);
         m2ExDescription.setText("Placeholder description");
-        m2ExDescription.setSize(640,60);
+        m2ExDescription.setSize(640,50);
         m2ExDescription.setHorizontalAlignment(JLabel.CENTER);
         m2ExDescription.setFont(new Font("Arial",Font.PLAIN,20));
 
         m2Counter = new JLabel();
         m2Counter.setText("Placeholder ct");
-        m2Counter.setSize(640,280);
-        m2Counter.setLocation(0,190);
+        m2Counter.setSize(640,240);
+        m2Counter.setLocation(0,180);
         m2Counter.setHorizontalAlignment(JLabel.CENTER);
         m2Counter.setVerticalAlignment(JLabel.CENTER);
         m2Counter.setFont(new Font("Arial",Font.PLAIN,100));
 
-        menu2 = new ArrayList<>(Arrays.asList(m2ExTitle,m2Counter,m2ExDescription));
+        m2Done = new JButton();
+        m2Done.setSize(600,40);
+        m2Done.setFont(m1FileInput.getFont());
+        m2Done.setText("Done.");
+        m2Done.setLocation(10,240+180);
+        m2Done.addActionListener(this);
+        m2Done.setVisible(false);
+
+
+
+        menu2 = new ArrayList<>(Arrays.asList(m2ExTitle,m2Counter,m2ExDescription,m2Done));
+        //for(int i=0;i<menu2.size();i++){
+        //    menu2.get(i).addKeyListener(KL);
+        //}
     }
 
     public static void switchMenu(int to){
@@ -174,5 +195,8 @@ public class InterfaceManager implements ActionListener{
             case M2_ExDescription: m2ExDescription.setText(updateTo);break;
         }
         window.update(window.getGraphics());
+    }
+    public static void EnableDisableDoneButton(boolean bool){
+        m2Done.setVisible(bool);
     }
 }
